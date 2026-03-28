eb9c30ed98dfaebd8632b1e1247e15cbcb335ee6
igorgp06 <igorgoncalvesp20@gmail.com>
2026-03-28 20:53:45 -0300

    [WORK 1.0.7] Melhorias na estrutura do Header com menu responsivo e adição do 'lucide-react'.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { cn } from "../lib/utils"
 import { useEffect, useState } from "react";
+import { Menu, X } from "lucide-react";
 
 const menuItems = [
     { name: "Início", to: "/" },
-    /* TODO: adicionar mais itens ao menu */
+    { name: "Produções", to: "/producoes" },
+    { name: "Contato", to: "/contato" },
 ]
 
 export const Header = () => {
@@ -36,18 +38,69 @@ export const Header = () => {
     return (
         <>
             <nav className={cn("fixed w-full top-0 left-0 z-40 transition-all duration-300",
-                isScrolled ? "bg-background/80 backdrop-blur-sm shadow-md" : "bg-transparent"
+                isScrolled ? "py-3 bg-background/80 backdrop-blur-md shadow-xs border-b" : "py-5"
             )}>
 
                 <div className="container flex items-center justify-between">
-                    <Link to="/"
-                    className="text-2xl font-bold text-primary-foreground">
-                        Ateliê da Ihara
+
+                    <Link to="/" className="text-2xl font-bold text-primary flex items-center" onClick={() => setIsMenuOpen(false)}>
+                        <span className="relative z-10 hover:text-hover transition-all duration-300">
+                            Ateliê da <span className="text-primary-foreground">Ihara</span>
+                        </span>
                     </Link>
 
-                </div>
+                    {/* desktop menu */}
 
+                    <div className="hidden md:flex space-x-8">
+                        {menuItems.map((item, key) => (
+                            <NavLink
+                                key={key}
+                                to={item.to}
+                                className={cn(
+                                    "text-lg font-medium text-primary transition-all duration-300",
+                                    "hover:opacity-80 hover:text-hover"
+                                )}
+                            >
+                                {item.name}
+                            </NavLink>
+                        ))}
+                    </div>
+                </div>
             </nav>
+
+            <button
+                onClick={() => setIsMenuOpen(prev => !prev)}
+                className="md:hidden text-primary fixed top-2 right-4 z-50 p-2 hover:opacity-80 transition-all duration-300"
+                aria-label={isMenuOpen ? "Fechar menu" : "Abrir menu"}>
+                {isMenuOpen ? <X size={24} /> : <Menu size={24} />}
+            </button>
+
+            <div className={cn(
+                "fixed inset-0 bg-background/90 backdrop-blur-md z-49 flex flex-col",
+                "items-center justify-center space-x-8 transition-all duration-300 md:hidden",
+                isMenuOpen
+                    ? "opacity-100 pointer-events-auto"
+                    : "opacity-0 pointer-events-none"
+            )}>
+
+                <div className="flex flex-col space-x-8 gap-8 text-xl text-primary w-full h-full justify-center">
+                    {menuItems.map((item, key) => (
+                        <NavLink
+                            key={key}
+                            to={item.to}
+                            className={({ isActive }) =>
+                                cn(
+                                    "text-center font-medium transition-all duration-300 m-0",
+                                    isActive
+                                        ? "text-primary-foreground"
+                                        : "hover:opacity-80 hover:text-hover"
+                                )} onClick={() => setIsMenuOpen(false)}>
+                            {item.name}
+                        </NavLink>
+                    ))}
+
+                </div>
+            </div>
         </>
     )
 }
